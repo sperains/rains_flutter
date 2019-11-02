bf53fbd91684a2cd6835b6926b6a765ce205bccf
Sperains <chenai0122@gmail.com>
2019-11-02 13:24:39 +0800

    posts列表

--- lib/main.dart
+++ lib/main.dart
@@ -24,14 +24,33 @@ class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
 
   Widget _listItemBuilder(BuildContext context, int index) {
-    return Text(
-      posts[index].title
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16,),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text('Sperains'),
         elevation: 0,
