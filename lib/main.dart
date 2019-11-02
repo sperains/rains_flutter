e4da0eba3971e7e67eca221e68280fe4a9344784
Sperains <chenai0122@gmail.com>
2019-11-02 14:57:13 +0800

    tab: default-tab-bar

--- lib/main.dart
+++ lib/main.dart
@@ -25,25 +25,41 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('Sperains'),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'navagation',
-          onPressed: () => debugPrint(' navigation is pressed'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Sperains'),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'navagation',
+            onPressed: () => debugPrint(' navigation is pressed'),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'search',
+              onPressed: () => debugPrint(' search is pressed'),
+            )
+          ],
+          elevation: 0,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist),),
+              Tab(icon: Icon(Icons.change_history),),
+              Tab(icon: Icon(Icons.directions_bike),),
+            ],
+          ),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'search',
-            onPressed: () => debugPrint(' search is pressed'),
-          )
-        ],
-        elevation: 0,
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128,),
+            Icon(Icons.change_history, size: 128,),
+            Icon(Icons.directions_bike, size: 128,),
+          ],
+        )
       ),
-      body: null
     );
   }
 }
