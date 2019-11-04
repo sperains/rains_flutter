b432522f7cc5c1deb8ad8fd5401c8b4305e7ce0a
Sperains <chenai0122@gmail.com>
2019-11-04 23:57:51 +0800

    Buttons and shape

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -6,7 +6,45 @@ class BasicDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ContainerBoxDecorationDemo();
+    return Container(
+      // color: Colors.lightGreen,
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints.expand(height: 300, width: 300),
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+              child: Text('FlatButton'),
+              onPressed: () => {},
+              color: Colors.lightBlueAccent,
+            ),
+            OutlineButton(
+              child: Text('OutLineButton', style: TextStyle(color: Colors.redAccent),),
+              onPressed: () => {},
+              color: Colors.greenAccent,
+            ),
+            Container(
+              height: 50,
+              width: 120,
+              child: RaisedButton(
+                child: Text("raise button"),
+                onPressed: () => {},
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.redAccent[100]
+              ),
+            ),
+            
+            FloatingActionButton(
+              child: Icon(Icons.airport_shuttle),
+              onPressed: () => {},
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
